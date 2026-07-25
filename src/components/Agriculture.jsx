88d94a95661e6165290c80621e71a424f720992a
Akeem <quadriakeembab@gmail.com>
2026-07-25 17:40:24 +0100

    Agriculture image updated

--- src/components/Agriculture.jsx
+++ src/components/Agriculture.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
-import farmerpicture from "../assets/Farmer.jpg";
+import farmerpicture from "../assets/Farmer.png";
 
 const Agriculture = () => {
   return (
